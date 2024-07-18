cc949d694376ad23b87713f18e18d37d60234447
Zackdokong <66398105+Zackdokong@users.noreply.github.com>
2024-07-19 08:21:36 +0900

    Finished #2.2 Loading Screen

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import CreateAccount from "./routes/create-account";
 import Login from "./routes/login";
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./components/loading-screen";
 
 const router = createBrowserRouter([
   {
@@ -45,10 +47,23 @@ const GlobalStyles = createGlobalStyle`
 `;
 
 function App() {
+const [isLoading, setIsLoading] = useState(true);
+
+const init = async() => {
+  //firebase code
+
+  setIsLoading(false);
+};
+
+useEffect(() => {
+  init();
+}, []);
+
+
   return(
     <>
-    <GlobalStyles />
-      <RouterProvider router={router} />
+      <GlobalStyles />
+      {isLoading ? <LoadingScreen /> : <RouterProvider router = {router} />}  
     </>
   );
 }
