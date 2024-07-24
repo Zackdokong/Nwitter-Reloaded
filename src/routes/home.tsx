0b5cc9bd69655d716387ff271a127fe68f937ed5
Zackdokong <66398105+Zackdokong@users.noreply.github.com>
2024-07-24 22:15:08 +0900

    Finished #4.6 Deleting Tweets

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -3,17 +3,20 @@ import PostTweetForm from "../components/post-tweet-form";
 import Timeline from "../components/timeline";
 
 const Wrapper = styled.div`
-    display: grid;
-    gap: 50px;
-    overflow-y: scroll;
-    grid-template-rows: 1fr 5fr;
+  display: grid;
+  gap: 50px;
+  overflow-y: scroll;
+  grid-template-rows: 1fr 5fr;
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 export default function Home() {
-    return (
-        <Wrapper>
-            <PostTweetForm />
-            <Timeline />
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <PostTweetForm />
+      <Timeline />
+    </Wrapper>
+  );
 }
