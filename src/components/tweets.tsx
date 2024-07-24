387032ab218ff7e0bfe81b37213b8373c2ca156a
Zackdokong <66398105+Zackdokong@users.noreply.github.com>
2024-07-24 21:58:29 +0900

    Finished #4.5 Realtime Tweets

--- src/components/tweets.tsx
+++ src/components/tweets.tsx
@@ -7,7 +7,6 @@ const Wrapper = styled.div`
     padding: 20px;
     border: 1px solid rgba(255,255,255, 0.5);
     border-radius: 15px; 
-    margin: 10px 0px;
 `;
 
 const Column = styled.div``;
@@ -35,11 +34,11 @@ export default function Tweet({ username, photo, tweet }: ITweet) {
         <Username>{username}</Username>
         <Payload>{tweet}</Payload>
       </Column>
+      <Column>
       {photo ? (
-        <Column>
           <Photo src={photo} />
-        </Column>
-      ) : null}
+      ) : null
+      }</Column>
     </Wrapper>
   );
 }
